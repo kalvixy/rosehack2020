1209913c5ebc4c5ee308021f7ba4b3663c83bc18
Katie Fukuda <fukudakatie@gmail.com>
2020-01-18 21:35:07 -0800

    level 1 map and collisions

---
+++ Assets/Move_To_Next_Level.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Move_To_Next_Level : MonoBehaviour {
+
+    [SerializeField] private string nextLevel;
+
+    void OnTriggerEnter2D(Collider2D witch)
+    {
+        Debug.Log("collided...");
+        if (witch.CompareTag("Player")) {
+            Debug.Log("collided with player");
+            SceneManager.LoadScene(nextLevel);
+        }
+        
+    }
+}
